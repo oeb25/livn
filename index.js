b424dcbe24013fbee5e57d2d3165fdc039aa4dda
Oliver Bøving <lvrmlbvng@gmail.com>
2015-06-01 18:51:10 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('babel/register');
+
+var browserify = require('browserify-middleware');
+
+module.exports = function(app) {
+	app.use('/', browserify('./', {
+		cache: false,
+		transform: ['babelify']
+	}));
+
+	return function(req, res, next) {
+		next();
+	}
+}
